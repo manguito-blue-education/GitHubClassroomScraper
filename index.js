1ab56c224981959e912af8e3c19daa8d72e7e2c8
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-04-21 16:32:25 -0500

    Added login functionality to scrapper

--- index.js
+++ index.js
@@ -1,35 +1,72 @@
+// Para copiar las urls del classroom ejecuta lo siguiente en la consola
+// copy([...document.getElementsByTagName("h3")].map(item => item.children[0].href));
 const puppeteer = require("puppeteer");
 
 process.setMaxListeners(Infinity);
 
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+
+const activityUrl = process.argv.slice(2)[0] || "";
+
 const main = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(240000);
     await page.setViewport({ width: 1920, height: 926 });
 
-    await page.goto(
-      "https://classroom.github.com/classrooms/47409156-prepadawans-gen-2"
-    );
+    await page.goto(activityUrl);
 
-    const cookies = [
-      { name: "_octo", value: "GH1.1.349947777.1585017639" },
-      { name: "_ga", value: "GA1.2.918775446.1585018562" },
-      { name: "tz", value: "America%2FMexico_City" },
-    ];
+    await page.type("#login_field", process.env.GH_EMAIL || "your_email");
+    await page.type("#password", process.env.GH_PASSWORD || "your_pswd");
 
-    await page.setCookie(...cookies);
+    await Promise.all([
+      page.click(".btn.btn-primary.btn-block"),
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+    ]);
+
+    await page.type("#otp", process.env.GH_OTP_PASSWORD || "your_otp");
+
+    await Promise.all([
+      page.click(".btn.btn-primary.btn-block"),
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+    ]);
+
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
 
     await page.screenshot({ path: "buddy-screenshot.png" });
 
     const availableActivities = await page.evaluate(() => {
-      return [...document.getElementsByTagName("h3")];
+      const activityTitle = document.getElementsByTagName("h1")[0].innerText;
+      const users = [
+        ...document.getElementsByClassName("assignment-repo-list-item"),
+      ].map((item) => {
+        const userName = item.children[0].children[1].children[0].innerText;
+        const description = item.children[0].children[1].children[1].innerText
+          .trim()
+          .split("\n")
+          .join(" ");
+        return { userName, description };
+      });
+
+      return users.map((user) => ({ ...user, activityTitle }));
     });
 
-    console.log(availableActivities);
-
     await browser.close();
+
+    const csvWriter = createCsvWriter({
+      path: `./${availableActivities[0].activityTitle
+        .trim()
+        .split(" ")
+        .join("_")}.csv`,
+      header: [
+        { id: "activityTitle", title: "Actividad" },
+        { id: "userName", title: "Usuario" },
+        { id: "description", title: "Descripción" },
+      ],
+    });
+
+    await csvWriter.writeRecords(availableActivities);
   } catch (error) {
     console.log("valió vrg", error);
   }
---
+++ main.sh
@@ -0,0 +1,5 @@
+ACTIVIDADES=(https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-1-hello-world-js https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-2-geometry-js https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-3-array-of-multiples https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-4-get-budget https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-5-broken-keyboard-js)
+for actividad in "${ACTIVIDADES[@]}"
+do
+	echo $actividad
+done
