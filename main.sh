268b5bcbbc27fe696ff89adb12761b522cad14c5
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-05-29 15:54:32 -0500

    Modified single file

--- main.sh
+++ main.sh
@@ -2,7 +2,8 @@
 # For getting activities link, use the following snippet inside browser console
 # Para copiar las urls del classroom ejecuta lo siguiente en la consola
 # copy([...document.getElementsByTagName("h3")].map(item => item.children[0].href));
-ACTIVIDADES=(https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-1-hello-world-js https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-2-geometry-js https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-3-array-of-multiples https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-4-get-budget https://classroom.github.com/classrooms/47409156-prepadawans-gen-2/assignments/activity-5-broken-keyboard-js)
+ACTIVIDADES=(https://classroom.github.com/classrooms/47409156-prepadawans-gen-3/assignments/activity-6-highest-ocurrence-python)
+
 for actividad in "${ACTIVIDADES[@]}"
 do
 	node index.js $actividad
